e8335d29d80c72335c320a9fcba41aa228ea3152
Hipf02@gmail.com <E5a5ee11!>
2023-04-20 14:41:25 +0300

    3. Массивы и модели. [#174892] v0.1

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -11,12 +11,13 @@ public class Library {
         books[1] = money;
         books[2] = kolobok;
         books[3] = braveNewWorld;
+        String ln = System.lineSeparator();
 
         for (int i = 0; i < books.length; i++) {
             System.out.println(books[i].getName() + " - " + books[i].getPageCount());
         }
 
-        System.out.println("\n" + "Replace 0 to 3" + "\n");
+        System.out.println(ln + "Replace 0 to 3" + ln);
         Book temp = books[0];
         books[0] = books[3];
         books[3] = temp;
@@ -25,9 +26,9 @@ public class Library {
             System.out.println(books[i].getName() + " - " + books[i].getPageCount());
         }
 
-        System.out.println("\n" + "Shown only book Clean Code" + "\n");
+        System.out.println(ln + "Shown only book Clean Code" + ln);
         for (int i = 0; i < books.length; i++) {
-            if (books[i].getName().equals("Clean code")) {
+            if ("Clean code".equals(books[i].getName())) {
                 System.out.println(books[i].getName() + " - " + books[i].getPageCount());
             }
         }
