8e594fcb1ddeadd77271ff24e8f72ba13f9d0e2e
Hipf02@gmail.com <E5a5ee11!>
2023-04-20 17:34:53 +0300

    10. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,6 +5,8 @@ import java.time.format.DateTimeFormatter;
 public class StartUI {
 
     public static void main(String[] args) {
-        System.out.println(new Item().getCreated().format(DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss")));
+        Item item = new Item();
+        System.out.println(item.getCreated().format(DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss")));
+        System.out.println(item);
     }
 }
