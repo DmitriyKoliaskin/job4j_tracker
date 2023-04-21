c4dbbc9580f5d3808fcb4f697a17b2114792efbe
Hipf02@gmail.com <E5a5ee11!>
2023-04-21 10:37:41 +0300

    7. Метод удаления заявки Tracker.delete [#211749] v0.1

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -65,7 +65,7 @@ public class Tracker {
         int index = indexOf(id);
         boolean result = index != -1;
         if (result) {
-            System.arraycopy(items, index + 1, items, size - index - 1, index);
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
         }
