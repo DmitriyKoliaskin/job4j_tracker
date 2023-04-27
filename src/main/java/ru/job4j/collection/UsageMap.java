80a6579d1e83eedcd44eb8b4f3565dd9eb0725fd
Hipf02@gmail.com <E5a5ee11!>
2023-04-27 17:28:51 +0300

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,17 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("parsentev@yandex.ru", "Petr Arsentev Sergeevich");
+        map.put("hipf02@gmail.com", "Dmitriy Koliaskin Aleksandrovich");
+
+        for (var key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}
